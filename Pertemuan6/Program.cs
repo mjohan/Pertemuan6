b5a118a9ad1911dc9650c196045470d83fafd81a
mjohan <m.johan.alibasa@gmail.com>
2021-03-25 19:03:44 +1100

    * input.json:
    * Program.cs: An example of a more complicated json file

--- Pertemuan6/Program.cs
+++ Pertemuan6/Program.cs
@@ -5,12 +5,17 @@ using System.Collections.Generic;
 
 namespace Pertemuan6
 {
+    class NamaDosen {
+        public string Lengkap { get; set; }
+        public int Panggilan { get; set; }
+    }
+
     class Dosen {
-        public string Nama { get; set; }
+        public NamaDosen Nama { get; set; }
         public string NIP { get; set; }
         public string Prodi { get; set; }
         public string Status { get; set; }
-        public string Telepon { get; set; }
+        public List<string> Telepon { get; set; }
     }
 
     class Program
@@ -22,7 +27,12 @@ namespace Pertemuan6
             List<Dosen> listDosen = JsonSerializer.Deserialize<List<Dosen>>(hasilBaca);
 
             for (int i = 0; i < listDosen.Count; i++) {
-                Console.WriteLine( listDosen[i].Telepon);
+                Console.WriteLine(listDosen[i].Nama.Panggilan);
+
+                List<string> listTelepon = listDosen[i].Telepon;
+                for (int j = 0; j < listTelepon.Count; j++) {
+                    Console.WriteLine("No telp ke-{0} = {1}", j+1, listTelepon[j]);
+                }
             }
         }
     }
